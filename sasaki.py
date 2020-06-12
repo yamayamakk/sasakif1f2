b8fbc4df675bd17739ae10ac9b7b42483731bcd5
YamadaSP8 <kanatsuki14@gmail.com>
2020-06-12 15:54:58 +0900

    200610

---
+++ sasaki.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+import math
+import matplotlib
+#matplotlib.use('Agg')
+
+
+import matplotlib.pyplot as plt
+from scipy import interpolate
+
+tex = open("f1f2_Sasaki.dat")
+lines = tex.readlines()
+
+for i in range(len(lines)):
+    if (lines[i][0] == "#") and (lines[i][1] == "S"):
+        ll = lines[i].split()
+        AtomNum = int(ll[1])
+        outname = "f1f2_" + str(AtomNum) + ".dat"
+        out = open(outname, "w")
+        
+        for j in range(i+5, len(lines)):
+            if lines[j][0]=="#":
+                break
+            else:
+                out.write(lines[j])
+        out.close()
+        
